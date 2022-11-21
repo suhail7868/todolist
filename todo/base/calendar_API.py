cd54256f2f16a63a7fbe1fac15fa605c1f84f318
suhail7868 <63796357+suhail7868@users.noreply.github.com>
2022-11-21 15:10:27 +0530

    Add files via upload

---
+++ todo/base/calendar_API.py
@@ -0,0 +1,7 @@
+def test_calendar():
+    print("RUNNING TEST_CALENDAR()")
+    test_event1 = {"created","Title","description"}
+    test_event2 = {"created","Title","description"}
+    events = [test_event1, test_event2]
+
+    return events
